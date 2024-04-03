259889af07a46b766c8802ffed65ab57e500bfba
Abdulk777 <113355669+Abdulk777@users.noreply.github.com>
2024-04-03 22:00:33 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,12 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.rgname
+  location = var.rglocation
+}
+
+resource "azurerm_service_plan" "example" {
+  name                = var.planname
+  resource_group_name = var.rgname
+  location            = var.rglocation
+  os_type             = var.kind
+  sku_name            = "P1v2"
+}
